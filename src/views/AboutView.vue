7421d65d5e600f7c57371651b55bd876a5cece61
Achsmboh <maulana.achsanxiipa1@gmail.com>
2023-01-18 10:56:36 +0700

    add commponent layout

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <LayOut>
+      <template v-slot:children>
+        <div class="bungkus">
+          <div class="card"></div>
+        </div>
+      </template>
+    </LayOut>
   </div>
 </template>
+<style>
+/* .bungkus {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+}
+.about {
+  background-color: aquamarine;
+  width: 100%;
+  margin: 0;
+}
+p {
+  margin: 0;
+}
+.card {
+  width: 1034px;
+  height: 580px;
+  background-color: red;
+} */
+</style>
