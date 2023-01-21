07a4aa7e6defb9751ee3a3c7d987f8be9e352e78
Achsmboh <maulana.achsanxiipa1@gmail.com>
2023-01-21 17:34:16 +0700

    add detail background

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about">
-    <LayOut className="women">
+    <LayOut className="men">
       <template v-slot:children>
         <div class="container">
           <CardCatalog
             image="https://www.pngmart.com/files/1/Jacket-PNG-Clipart.png"
-            category="women"
+            category="men"
             title="Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket"
             description="100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON"
             price="29.95"
@@ -22,6 +22,9 @@
   height: 100vh;
   justify-content: center;
   align-items: center;
+  top: 0;
+  z-index: 999;
+  position: absolute;
 }
 .about {
   background-color: aquamarine;
