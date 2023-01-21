08e3b156658bf39dff05a79a01b50c5c5c9fa38b
Achsmboh <maulana.achsanxiipa1@gmail.com>
2023-01-21 12:08:38 +0700

    slicing page unvailable product

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import HomePageVue from "@/views/HomePage.vue";
+import UnavailableProductVue from "@/views/UnavailableProduct.vue";
 import Vue from "vue";
 import VueRouter from "vue-router";
 
@@ -9,6 +10,11 @@ const routes = [
     path: `/`,
     name: "HomePage",
     component: HomePageVue,
+  },
+  {
+    path: `*`,
+    name: "UnavailableProduct",
+    component: UnavailableProductVue,
   },
 ];
 
